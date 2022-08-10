222b61d9c5c6788e580ed2f5bf5d181769742377
Cristian Loaiza <kla-1997@hotmail.com>
2022-08-10 18:41:08 -0500

    First commit

---
+++ src/main/java/com/test/veterinary/clinic/Application.java
@@ -0,0 +1,13 @@
+package com.test.veterinary.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
