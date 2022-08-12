ccebd85f4a159431fdd2c402de09bf2a2516f131
Cristian Loaiza <kla-1997@hotmail.com>
2022-08-11 20:31:33 -0500

    Update migration

--- src/main/resources/db/migration/V1__20220810_create_db.sql
+++ src/main/resources/db/migration/V1__20220810_create_db.sql
@@ -1,254 +1,108 @@
---
--- PostgreSQL database dump
---
+CREATE SCHEMA IF NOT EXISTS veterinary_clinic
+    AUTHORIZATION test_user;
 
--- Dumped from database version 13.4
--- Dumped by pg_dump version 13.4
+CREATE TABLE IF NOT EXISTS veterinary_clinic.users
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    first_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    document_type character(3) COLLATE pg_catalog."default" NOT NULL,
+    document_number integer NOT NULL,
+    status integer NOT NULL,
+    gender "char" NOT NULL,
+    CONSTRAINT "PK_USERS" PRIMARY KEY (id),
+    CONSTRAINT "UK_USERS_DOCUMENT_NUMBER" UNIQUE (document_number)
+    )
 
--- Started on 2022-08-10 19:45:37
+    TABLESPACE pg_default;
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+ALTER TABLE veterinary_clinic.users
+    OWNER to test_user;
 
---
--- TOC entry 8 (class 2615 OID 25830)
--- Name: veterinary_clinic; Type: SCHEMA; Schema: -; Owner: test_user
---
+GRANT ALL ON TABLE veterinary_clinic.users TO test_user;
 
-CREATE SCHEMA IF NOT EXISTS veterinary_clinic;
+CREATE TABLE IF NOT EXISTS veterinary_clinic.workers
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    first_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    position_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    speciality character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    document_type character(3) COLLATE pg_catalog."default" NOT NULL,
+    document_number integer NOT NULL,
+    CONSTRAINT workers_pkey PRIMARY KEY (id)
+    )
 
+    TABLESPACE pg_default;
 
-ALTER SCHEMA veterinary_clinic OWNER TO test_user;
+ALTER TABLE veterinary_clinic.workers
+    OWNER to test_user;
 
-SET default_tablespace = '';
+GRANT ALL ON TABLE veterinary_clinic.workers TO test_user;
 
-SET default_table_access_method = heap;
+CREATE TABLE IF NOT EXISTS veterinary_clinic.pets
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    breed character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    gender "char" NOT NULL,
+    user_id integer NOT NULL,
+    CONSTRAINT "PK_PETS" PRIMARY KEY (id),
+    CONSTRAINT "FK_USERS_PETS_USER_ID" FOREIGN KEY (user_id)
+    REFERENCES veterinary_clinic.users (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    )
 
---
--- TOC entry 225 (class 1259 OID 25856)
--- Name: clinic_histories; Type: TABLE; Schema: veterinary_clinic; Owner: test_user
---
+    TABLESPACE pg_default;
 
-CREATE TABLE IF NOT EXISTS veterinary_clinic.clinic_histories (
-    id integer NOT NULL,
+ALTER TABLE veterinary_clinic.pets
+    OWNER to test_user;
+
+GRANT ALL ON TABLE veterinary_clinic.pets TO test_user;
+
+CREATE TABLE IF NOT EXISTS veterinary_clinic.clinic_histories
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     pet_id integer NOT NULL,
-    created_at date NOT NULL
-);
+    created_at date NOT NULL,
+    CONSTRAINT clinic_histories_pkey PRIMARY KEY (id)
+    )
 
+    TABLESPACE pg_default;
 
-ALTER TABLE veterinary_clinic.clinic_histories OWNER TO test_user;
+ALTER TABLE veterinary_clinic.clinic_histories
+    OWNER to test_user;
 
---
--- TOC entry 229 (class 1259 OID 25881)
--- Name: clinic_histories_detail; Type: TABLE; Schema: veterinary_clinic; Owner: test_user
---
+GRANT ALL ON TABLE veterinary_clinic.clinic_histories TO test_user;
 
-CREATE TABLE IF NOT EXISTS veterinary_clinic.clinic_histories_detail (
-    id integer NOT NULL,
+CREATE TABLE IF NOT EXISTS veterinary_clinic.clinic_histories_detail
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     temperature numeric(19,2) NOT NULL,
     weight numeric(19,2) NOT NULL,
     heart_rate numeric(19,2) NOT NULL,
     breathing_rate numeric(19,2) NOT NULL,
     date_time timestamp with time zone NOT NULL,
-    feeding character varying(255) NOT NULL,
-    habitat character varying(255) NOT NULL,
-    observations character varying(255) NOT NULL,
+                            feeding character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    habitat character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    observations character varying(255) COLLATE pg_catalog."default" NOT NULL,
     worker_id integer NOT NULL,
-    clinic_history_id integer NOT NULL
-);
+    clinic_history_id integer NOT NULL,
+    CONSTRAINT clinic_histories_detail_pkey PRIMARY KEY (id),
+    CONSTRAINT "FK_CH_CLINIC_HISTORY_ID" FOREIGN KEY (clinic_history_id)
+    REFERENCES veterinary_clinic.clinic_histories (id) MATCH SIMPLE
+                        ON UPDATE NO ACTION
+                        ON DELETE NO ACTION,
+    CONSTRAINT "FK_WORKERS_CH_DETAIL_WORKER_ID" FOREIGN KEY (worker_id)
+    REFERENCES veterinary_clinic.workers (id) MATCH SIMPLE
+                        ON UPDATE NO ACTION
+                        ON DELETE NO ACTION
+    )
 
+    TABLESPACE pg_default;
 
-ALTER TABLE veterinary_clinic.clinic_histories_detail OWNER TO test_user;
+ALTER TABLE veterinary_clinic.clinic_histories_detail
+    OWNER to test_user;
 
---
--- TOC entry 226 (class 1259 OID 25861)
--- Name: clinic_histories_id_seq; Type: SEQUENCE; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE veterinary_clinic.clinic_histories ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME veterinary_clinic.clinic_histories_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
-
---
--- TOC entry 223 (class 1259 OID 25841)
--- Name: pets; Type: TABLE; Schema: veterinary_clinic; Owner: test_user
---
-
-CREATE TABLE IF NOT EXISTS veterinary_clinic.pets (
-    id integer NOT NULL,
-    name character varying(255) NOT NULL,
-    breed character varying(255) NOT NULL,
-    gender "char" NOT NULL,
-    user_id integer NOT NULL
-);
-
-
-ALTER TABLE veterinary_clinic.pets OWNER TO test_user;
-
---
--- TOC entry 224 (class 1259 OID 25854)
--- Name: pets_id_seq; Type: SEQUENCE; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE veterinary_clinic.pets ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME veterinary_clinic.pets_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
-
---
--- TOC entry 222 (class 1259 OID 25831)
--- Name: users; Type: TABLE; Schema: veterinary_clinic; Owner: test_user
---
-
-CREATE TABLE IF NOT EXISTS veterinary_clinic.users (
-    id integer NOT NULL,
-    first_name character varying(255) NOT NULL,
-    last_name character varying(255) NOT NULL,
-    document_type character(3) NOT NULL,
-    document_number integer NOT NULL,
-    status integer NOT NULL,
-    gender "char" NOT NULL
-);
-
-
-ALTER TABLE veterinary_clinic.users OWNER TO test_user;
-
---
--- TOC entry 228 (class 1259 OID 25865)
--- Name: workers; Type: TABLE; Schema: veterinary_clinic; Owner: test_user
---
-
-CREATE TABLE IF NOT EXISTS veterinary_clinic.workers (
-    id integer NOT NULL,
-    first_name character varying(255) NOT NULL,
-    last_name character varying(255) NOT NULL,
-    position_name character varying(255) NOT NULL,
-    speciality character varying(255) NOT NULL,
-    document_type character(3) NOT NULL,
-    document_number integer NOT NULL
-);
-
-
-ALTER TABLE veterinary_clinic.workers OWNER TO test_user;
-
---
--- TOC entry 227 (class 1259 OID 25863)
--- Name: workers_id_seq; Type: SEQUENCE; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE veterinary_clinic.workers ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME veterinary_clinic.workers_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
-
---
--- TOC entry 2939 (class 2606 OID 25848)
--- Name: pets PK_PETS; Type: CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.pets
-    ADD CONSTRAINT "PK_PETS" PRIMARY KEY (id);
-
-
---
--- TOC entry 2935 (class 2606 OID 25838)
--- Name: users PK_USERS; Type: CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.users
-    ADD CONSTRAINT "PK_USERS" PRIMARY KEY (id);
-
-
---
--- TOC entry 2937 (class 2606 OID 25840)
--- Name: users UK_USERS_DOCUMENT_NUMBER; Type: CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.users
-    ADD CONSTRAINT "UK_USERS_DOCUMENT_NUMBER" UNIQUE (document_number);
-
-
---
--- TOC entry 2945 (class 2606 OID 25888)
--- Name: clinic_histories_detail clinic_histories_detail_pkey; Type: CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.clinic_histories_detail
-    ADD CONSTRAINT clinic_histories_detail_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 2941 (class 2606 OID 25860)
--- Name: clinic_histories clinic_histories_pkey; Type: CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.clinic_histories
-    ADD CONSTRAINT clinic_histories_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 2943 (class 2606 OID 25872)
--- Name: workers workers_pkey; Type: CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.workers
-    ADD CONSTRAINT workers_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 2948 (class 2606 OID 25894)
--- Name: clinic_histories_detail FK_CH_CLINIC_HISTORY_ID; Type: FK CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.clinic_histories_detail
-    ADD CONSTRAINT "FK_CH_CLINIC_HISTORY_ID" FOREIGN KEY (clinic_history_id) REFERENCES veterinary_clinic.clinic_histories(id);
-
-
---
--- TOC entry 2946 (class 2606 OID 25849)
--- Name: pets FK_USERS_PETS_USER_ID; Type: FK CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.pets
-    ADD CONSTRAINT "FK_USERS_PETS_USER_ID" FOREIGN KEY (user_id) REFERENCES veterinary_clinic.users(id);
-
-
---
--- TOC entry 2947 (class 2606 OID 25889)
--- Name: clinic_histories_detail FK_WORKERS_CH_DETAIL_WORKER_ID; Type: FK CONSTRAINT; Schema: veterinary_clinic; Owner: test_user
---
-
-ALTER TABLE ONLY veterinary_clinic.clinic_histories_detail
-    ADD CONSTRAINT "FK_WORKERS_CH_DETAIL_WORKER_ID" FOREIGN KEY (worker_id) REFERENCES veterinary_clinic.workers(id);
-
-
--- Completed on 2022-08-10 19:45:38
-
---
--- PostgreSQL database dump complete
---
-
+GRANT ALL ON TABLE veterinary_clinic.clinic_histories_detail TO test_user;
